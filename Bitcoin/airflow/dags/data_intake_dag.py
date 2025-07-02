20e889a3efa19ef1ff332ba90ba0c0f2e79a0eee
Stephen Baraik <stephenbaraik@gmail.com>
2025-07-02 23:52:53 +0530

    added new files and codes

---
+++ Bitcoin/airflow/dags/data_intake_dag.py
@@ -0,0 +1,30 @@
+from airflow import DAG
+from airflow.operators.docker_operator import DockerOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 1, 1),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+with DAG(
+    'bitcoin_data_intake',
+    default_args=default_args,
+    schedule_interval='@daily',  # Runs daily at midnight UTC
+    catchup=False
+) as dag:
+
+    fetch_binance_data = DockerOperator(
+        task_id='fetch_binance_ohlc',
+        image='binance_fetcher',
+        api_version='auto',
+        auto_remove=True,
+        environment={
+            'DB_HOST': 'postgres',
+            'DB_NAME': 'bitcoin_db',
+            'DB_USER': 'bitcoinuser',
+            'DB_PASS': 'bitcoinpass'
+        }
+    )
