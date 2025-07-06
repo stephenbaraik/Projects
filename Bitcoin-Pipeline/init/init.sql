3f9fa2824236363710df7e5ccfe2f79a5a181e88
Stephen Baraik <stephenbaraik@gmail.com>
2025-07-06 08:38:05 +0530

    new docker yml file

--- Bitcoin-Pipeline/init/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS btc_price (
-    id SERIAL PRIMARY KEY,
-    timestamp BIGINT,
-    open NUMERIC,
-    high NUMERIC,
-    low NUMERIC,
-    close NUMERIC,
-    volume NUMERIC
-);
